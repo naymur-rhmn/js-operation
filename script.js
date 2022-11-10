75c43aaf7b758e2aa2845d3245ce9fad2d1678f6
naymur <naymurmn@gmail.com>
2022-11-10 12:03:54 +0600

    Array methods

--- script.js
+++ script.js
@@ -201,9 +201,60 @@ console.log(names);
 */
 
 // loop in array 
+/*
 var num = [10, 12, 14, 16, 18, 20, 22, 24, 26, 28];
 for (i = 0; i < num.length; i++) {
   var x = num[i];
   document.write(' ' + x);
   console.log(x);
 } 
+*/
+
+
+// array methods
+var names = ['Anis', 'Rohim', 'Korim', 'Badal', 'Rabbi'];
+
+// push method array te last element add kore
+names.push('Sojib');
+
+// pop method array er last elemetn remove kore
+names.pop();
+
+// shift method (array er prothom theeke element remove kore)
+names.shift();
+
+// unshift method (array er prothom e element add kore)
+names.unshift('Komola');
+
+// splice method diye array te element remove and add kora jay
+// add element using splice method
+names.splice(1, 0, 'Parvej', 'Kamrul');
+
+// remove element using splice method
+// prothom songkha index target kore, ditiyo songkha kotogulo element remove korbe target kore
+names.splice(2, 1);
+
+// slice method returns selected elements in an array, as a new array
+// slice method does not change the original array
+// slice method selects from a given start, up to a given end
+
+var newArr = names.slice(2, 5)
+
+// negative value works from end
+var newArr = names.slice(-5, -1)
+
+// sort method alphabet order e array element show kore
+names.sort()
+
+// reverse method alphabet reverse order e element show kore
+names.reverse()
+
+
+// number sort in an array
+var num = [3, 1, 22, 12, 9, 7, 30, 21]
+num.sort(function (a, b) {
+  // a - b coto theke boro songkha dekhabe, b - a boro theke coto songkha dekhabe (smile)
+  return a - b;
+})
+
+console.log(num);
